85abe997bf952765fc7a0fba34fcb68bf4fa82ce
woojungyang <blossom1113@nate.com>
2021-10-15 16:07:50 +0900

    애니메이션 작업중

---
+++ src/components/List/PaginationCp.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="wrapper list-wrapper">
+    <TableCp />
+  </section>
+</template>
+
+<script>
+import TableCp from "@/components/common/TableCp.vue";
+
+export default {
+  name: "PaginationCp",
+  components: { TableCp },
+  props: ["page"],
+  created() {
+    this.$store.dispatch("ACT_BOOKS", this.page);
+    this.$store.dispatch("ACT_LOADING", true);
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/List.vue
+++ src/views/List.vue
@@ -1,12 +1,20 @@
 <template>
-  <ListCp />
+  <PaginationCp :page="page || 1" />
 </template>
 
 <script>
-import ListCp from "../components/ListCp.vue";
+import PaginationCp from "../components/List/PaginationCp.vue";
 
 export default {
   name: "List",
-  components: { ListCp },
+  data() {
+    return {
+      page: this.$route.params.page,
+    };
+  },
+  components: { PaginationCp },
+  created() {
+    this.page = this.$route.params.page;
+  },
 };
 </script>
