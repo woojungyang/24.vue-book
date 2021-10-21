bcb3192307ed1dc08204145f1e7d0cdad7edcd37
woojungyang <blossom1113@nate.com>
2021-10-21 13:02:23 +0900

    주소 오류 수정

--- src/views/ListInfinite.vue
+++ src/views/ListInfinite.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import InfiniteCp from "../components/Infinite/InfiniteCp.vue";
+import InfiniteCp from "@/components/infinite/InfiniteCp.vue";
 
 export default {
   name: "ListInfinite",
